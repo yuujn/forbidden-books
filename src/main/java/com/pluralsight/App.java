2a21e96166d0d08b51bd202695a69cb9f370b29a
yuujn <pale.cup5223@fastmail.com>
2026-04-29 09:40:04 -0500

    sketch out home menu

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,15 +1,48 @@
 package com.pluralsight;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
+    static Scanner scan = new Scanner(System.in);
+    static Database database;
     public static void main(String[] args) {
-        Database db;
         try {
-             db = Database.openDatabase("transactions.csv");
+            database = Database.openDatabase("transactions.csv");
         } catch (IOException e) {
             System.out.println("Failed to open transactions database.");
             throw new RuntimeException(e);
+        }
+
+        runHomeMenu();
+    }
+
+    static void printHomeMenuOptions() {
+        System.out.println("D) Add Deposit");
+        System.out.println("P) Make Payment (Debit)");
+        System.out.println("L) Ledger");
+        System.out.println("X) Exit");
+    }
+
+    static void runHomeMenu() {
+        System.out.println("# Home");
+        System.out.println("=======");
+        boolean isRunning = true;
+
+        while (isRunning) {
+            printHomeMenuOptions();
+            System.out.print("Choose: ");
+            String choice = scan.next();
+
+            switch (choice.toLowerCase()) {
+                case "d" -> {}
+                case "p" -> {}
+                case "l" -> {}
+                case "x" -> isRunning = false;
+                default -> {
+                    System.out.println("Please enter one of D, P, L, or X.");
+                }
+            }
         }
     }
 }
